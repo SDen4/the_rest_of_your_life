69ace04a0d88aaf51a53401caa08d493b828bf52
SDen4 <asw325@yandex.ru>
2022-01-03 16:39:34 +0500

    form title constants

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
-import { formNames } from '../../constants/form';
+import { formNames, formTitle } from '../../constants/form';
 
 import styles from './Form.module.css';
 
 const Form: React.FC = () => {
   return (
-    <form className={styles.formWrapper}>
-      <label>{formNames.rus}</label>
-      <input type="text" />
-    </form>
+    <>
+      <h2>{formTitle.rus}</h2>
+      <form className={styles.formWrapper}>
+        <label>{formNames.rus}</label>
+        <input type="text" />
+      </form>
+    </>
   );
 };
 
--- src/constants/form.ts
+++ src/constants/form.ts
@@ -2,3 +2,8 @@ export const formNames = {
   rus: 'Страна',
   eng: 'Country',
 };
+
+export const formTitle = {
+  rus: 'Пожалуйста, заполните поля',
+  eng: 'Please, fill the form',
+};
