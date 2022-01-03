d364b88320e8dc7b63c709cb6940122a9b577993
SDen4 <asw325@yandex.ru>
2022-01-03 14:38:13 +0500

    disclaimer text, styles, and structure

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Disclaimer from '../Disclaimer';
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.appWrapper}>
       <Disclaimer />
     </div>
   );
--- src/components/Disclaimer/Disclaimer.tsx
+++ src/components/Disclaimer/Disclaimer.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import styles from './Disclaimer.module.css';
+import { DisclaimerText } from '../../constants/disclaimerText';
 
 const Disclaimer = () => {
-  return <div>Disclaimer</div>;
+  return (
+    <div className={styles.disclaimerText}>
+      {DisclaimerText.rus.split('.').map((el) => {
+        if (!el.length) return null;
+
+        const text = `${el.trim()}.`;
+
+        return (
+          <div key={el}>
+            <br />
+            <p>{text}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Disclaimer;
