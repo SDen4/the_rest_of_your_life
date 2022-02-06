b71ff98903464b8a5303e110995ec1eeb23dc6bc
SDen4 <asw325@yandex.ru>
2022-02-06 12:35:05 +0500

    feat:add rest in years in Result by click on the WeeksTable

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 
-import { resultButton } from '../../constants/result';
+import { resultButton, resultTextYears } from '../../constants/result';
 
 import { openForm, openResult } from '../../store/MainReducer/actions';
 
@@ -21,11 +21,34 @@ const Result: React.FC<IResult> = ({ store }) => {
     dispatch(openForm(true));
   };
 
+  const [show, setShow] = useState<boolean>(false);
+
+  const changeResultInfo = () => {
+    setShow(!show);
+  };
+
+  const height = `${store.valueYears * 7 - 1}px`;
+
   return (
     <div className={styles.resultWrapper}>
       <ResultText store={store} />
-      <div className={styles.tableWrapper}>
-        <WeeksTable valueYears={store.valueYears} userYears={store.userYears} />
+      <div className={styles.tableWrapper} onClick={changeResultInfo}>
+        {!show && (
+          <WeeksTable
+            valueYears={store.valueYears}
+            userYears={store.userYears}
+          />
+        )}
+
+        {show && (
+          <div className={styles.showInfoWrapper} style={{ height: height }}>
+            <h2>
+              Тебе осталось прожить примерно
+              <span>{(store.valueYears - store.userYears).toFixed(1)}</span>
+              {resultTextYears[store.currentLang]}
+            </h2>
+          </div>
+        )}
       </div>
 
       <Button
